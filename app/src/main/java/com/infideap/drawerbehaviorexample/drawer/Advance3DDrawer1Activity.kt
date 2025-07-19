c5fe7088c6f9bbd4ff2f78f3d7a25cabe0bc1ecb
iTech Panel <ramniwas8824@gmail.com>
2025-07-19 17:57:02 +0530

    applied changes for android 36

--- app/src/main/java/com/infideap/drawerbehaviorexample/drawer/Advance3DDrawer1Activity.kt
+++ app/src/main/java/com/infideap/drawerbehaviorexample/drawer/Advance3DDrawer1Activity.kt
@@ -8,9 +8,11 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
+
 import com.infideap.drawerbehavior.Advance3DDrawerLayout
 import com.infideap.drawerbehaviorexample.R
 
